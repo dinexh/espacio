6c1325c218673f432709bfc760073ebf13961c45
Dinesh Korukonda <dineshkorukonda05@gmail.com>
2024-07-05 05:18:49 +0530

    added modal for contact

--- App/script.js
+++ App/script.js
@@ -1,1 +1,17 @@
+var modal = document.getElementById('mymodal');
+var btn = document.getElementById("modal");
+var span = document.getElementsByClassName("close")[0];
 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
